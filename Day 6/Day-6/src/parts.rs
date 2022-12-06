00ba906bc0c7ef46f437b43f858c2109931ab492
Toby Bridle <foreignspaghettibolognese@gmail.com>
2022-12-06 17:08:46 +0000

    refactor: Cleaned up Day 6 by moving sliding window into function

--- Day 6/Day-6/src/parts.rs
+++ Day 6/Day-6/src/parts.rs
@@ -1,11 +1,12 @@
 use std::collections::{HashSet, VecDeque};
-pub fn part_one(line: &str) -> u32 {
+
+pub fn first_unique_window(line: &str, n: usize) -> u32 {
     let mut chars = line.chars();
     let mut chars = chars.by_ref().enumerate();
     // Fixed length of 4 characters. Pushing one pops the other end
-    let mut window: VecDeque<char> = VecDeque::with_capacity(4);
+    let mut window: VecDeque<char> = VecDeque::with_capacity(n);
     while let Some((i, c)) = chars.next() {
-        if i < 4 {
+        if i < n {
             window.push_back(c);
         } else {
             window.pop_front();
@@ -16,7 +17,7 @@ pub fn part_one(line: &str) -> u32 {
             .into_iter()
             .collect::<HashSet<char>>()
             .len()
-            == 4
+            == n
         {
             return i as u32 + 1;
         }
@@ -24,27 +25,10 @@ pub fn part_one(line: &str) -> u32 {
     0
 }
 
+pub fn part_one(line: &str) -> u32 {
+    return first_unique_window(line, 4);
+}
+
 pub fn part_two(line: &str) -> u32 {
-    let mut chars = line.chars();
-    let mut chars = chars.by_ref().enumerate();
-    // Fixed length of 4 characters. Pushing one pops the other end
-    let mut window: VecDeque<char> = VecDeque::with_capacity(14);
-    while let Some((i, c)) = chars.next() {
-        if i < 14 {
-            window.push_back(c);
-        } else {
-            window.pop_front();
-            window.push_back(c);
-        }
-        if window
-            .to_owned()
-            .into_iter()
-            .collect::<HashSet<char>>()
-            .len()
-            == 14
-        {
-            return i as u32 + 1;
-        }
-    }
-    0
+    return first_unique_window(line, 14);
 }
